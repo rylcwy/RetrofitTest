d1e682c48a828d9463c803273b9410654085b313
rylcwy <rylcwy@gmail.com>
2018-12-29 16:24:19 +0800

    expandable textview

--- app/src/main/java/com/example/wangyu/retrofittest/BetaList.java
+++ app/src/main/java/com/example/wangyu/retrofittest/BetaList.java
@@ -1,14 +1,26 @@
 package com.example.wangyu.retrofittest;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.ScrollView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class BetaList extends AppCompatActivity {
     private List<Versions> versionsList=new ArrayList<>();
+
+    private ViewPager mViewPager;
+
+    private TabLayout mTabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +34,23 @@ public class BetaList extends AppCompatActivity {
     }
 
     private void initVersions(){
-            Versions version1=new Versions("2.0.7.4","504","日志日志日志日志日志日志日志","2018-12-03 18:30:32");
-            versionsList.add(version1);
-            Versions version2=new Versions("2.0.7.3","503","[新增功能] - 详情页社区下新增视频TAB [新增功能] - 详情页社区下新增视频TAB [新增功能] - 详情页社区下新增视频TAB [新增功能] - 详情页社区下新增视频TAB  "
-                    +"- 新增论坛内搜索 "+"- 推荐视频模块增加“换一组”" +"[优化体验] - 视频加载速度优化"+" - 夜间模式体验优化","2018-12-06 18:30:32");
-            versionsList.add(version2);
+            Versions version3=new Versions("2.0.7.3","503","Android provides a rich application framework that allows you to build innovative apps and games for mobile devices in a Java language environment. The documents listed in the left navigation provide details about how to build apps using Android's various APIs.","2018-12-06 18:30:32");
+            versionsList.add(version3);
+            Versions version4=new Versions("2.0.7.3","503","[新增功能] - 详情页社区下新增视频TAB [新增功能] - 详情页社区下新增视频TAB [新增功能] - 详情页社区下新增视频TAB [新增功能] - 详情页社区下新增视频TAB  "
+                +"- 新增论坛内搜索 "+"- 推荐视频模块增加“换一组”" +"[优化体验] - 视频加载速度优化"+" - 夜间模式体验优化","2018-12-06 18:30:32");
+            versionsList.add(version4);
+            Versions version5=new Versions("2.0.7.3","503","[新增功能] - 详情页社区下新增视频TAB [新增功能] - 详情页社区下新增视频TAB [新增功能] - 详情页社区下新增视频TAB [新增功能] - 详情页社区下新增视频TAB  "
+                +"- 新增论坛内搜索 "+"- 推荐视频模块增加“换一组”" +"[优化体验] - 视频加载速度优化"+" - 夜间模式体验优化","2018-12-06 18:30:32");
+            versionsList.add(version5);
+            Versions version6=new Versions("2.0.7.3","503","[新增功能] - 详情页社区下新增视频TAB [新增功能] - 详情页社区下新增视频TAB [新增功能] - 详情页社区下新增视频TAB [新增功能] - 详情页社区下新增视频TAB  "
+                +"- 新增论坛内搜索 "+"- 推荐视频模块增加“换一组”" +"[优化体验] - 视频加载速度优化"+" - 夜间模式体验优化","2018-12-06 18:30:32");
+            versionsList.add(version6);
+            Versions version7=new Versions("2.0.7.3","503","[新增功能] - 详情页社区下新增视频TAB [新增功能] - 详情页社区下新增视频TAB [新增功能] - 详情页社区下新增视频TAB [新增功能] - 详情页社区下新增视频TAB  "
+                +"- 新增论坛内搜索 "+"- 推荐视频模块增加“换一组”" +"[优化体验] - 视频加载速度优化"+" - 夜间模式体验优化","2018-12-06 18:30:32");
+            versionsList.add(version7);
 
     }
+
+
+
 }
--- app/src/main/java/com/example/wangyu/retrofittest/VersionAdapter.java
+++ app/src/main/java/com/example/wangyu/retrofittest/VersionAdapter.java
@@ -3,38 +3,74 @@ package com.example.wangyu.retrofittest;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.text.method.ScrollingMovementMethod;
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ScrollView;
 import android.widget.TextView;
+
+import com.ms.square.android.expandabletextview.ExpandableTextView;
 
 import java.util.List;
 
 public class VersionAdapter extends ArrayAdapter<Versions>{
     private int resourceId;
+    private final Context mContext;
+    private final SparseBooleanArray mCollapsedStatus;
+    private final String[] sampleStrings;
 
     public VersionAdapter(Context context, int textViewResourceId,List<Versions> objects){
         super(context,textViewResourceId,objects);
         resourceId=textViewResourceId;
+        mContext=context;
+        mCollapsedStatus = new SparseBooleanArray();
+        sampleStrings = mContext.getResources().getStringArray(R.array.sampleStrings);
 
+
+
+
+
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
     }
 
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         Versions versions=getItem(position);
-        View view= LayoutInflater.from(getContext()).inflate(resourceId,parent,false);
-        TextView versionName=(TextView)view.findViewById(R.id.version_name);
-        TextView versionCode=(TextView)view.findViewById(R.id.version_code);
-        TextView versionDate=(TextView)view.findViewById(R.id.version_date);
-        TextView versionDetail=(TextView)view.findViewById(R.id.version_detail);
-        Button download=(Button)view.findViewById(R.id.download);
-        versionCode.setText(versions.getVersionCode());
-        versionName.setText(versions.getVersionName());
-        versionDetail.setText(versions.getVersionDetail());
-        versionDate.setText(versions.getVersionDate());
-        return view;
+        final ViewHolder viewHolder;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.versions_item, parent, false);
+            viewHolder = new ViewHolder();
+            viewHolder.expandableTextView = (ExpandableTextView) convertView.findViewById(R.id.expand_text_view);
+            TextView versionName=(TextView)convertView.findViewById(R.id.version_name);
+            TextView versionCode=(TextView)convertView.findViewById(R.id.version_code);
+            TextView versionDate=(TextView)convertView.findViewById(R.id.version_date);
+            Button download=(Button)convertView.findViewById(R.id.download);
+            versionCode.setText(versions.getVersionCode());
+            versionName.setText(versions.getVersionName());
+            versionDate.setText(versions.getVersionDate());
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        viewHolder.expandableTextView.setText(sampleStrings[position], mCollapsedStatus, position);
+
+
+        return convertView;
+
     }
+
+    private static class ViewHolder{
+        ExpandableTextView expandableTextView;
+    }
+
 }
